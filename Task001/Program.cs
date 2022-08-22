86193f0735e57e990a8547641c41ec5507957bf0
Zunickx <Zunickx@gmail.com>
2022-08-22 23:10:34 +0300

    Task001 Определение четных чисел в рандомном массиве

---
+++ Task001/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
+
+int[] array = new int[5]; // ограничил кол-во элементов в будущем рандомном массиве до 5
+int k = 0;
+Random arrayRnd = new Random();
+
+Console.WriteLine("Вывести рандомный массив:");
+
+void ConclArr()
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = arrayRnd.Next(100, 999); // кладем в массив случайные целые числа 
+        Console.Write("{0} ", array[i]);
+    }
+}
+
+void EvenElem()
+{
+    foreach(int element in array)
+    {
+        if (element % 2 == 0)
+        {
+            k++;
+        }
+    }
+    Console.WriteLine("Количество четных элементов {0}", k);
+}
+
+ConclArr();
+EvenElem();
