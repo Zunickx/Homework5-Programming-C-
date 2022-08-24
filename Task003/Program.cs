74ee2bf572a9619963a6bc1a28ce8a65b35eafdb
Zunickx <Zunickx@gmail.com>
2022-08-24 22:02:05 +0300

    Task003 Нахождение разницы между макс и мин элементом массива

---
+++ Task003/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
+
+double[] arrNumbers = new double[7];
+Random arrRnd = new Random();
+double diff = 0;
+
+Console.WriteLine("Массив вещественных чисел: ");
+
+void PrintArrNumbers()
+{
+    for(int i = 0; i < arrNumbers.Length; i++)
+    {
+        arrNumbers[i] = arrRnd.Next(-10, 10);
+        Console.Write("{0} ", arrNumbers[i]);
+    }
+    return;
+}
+
+void DifferenceNum()
+{
+    var max = arrNumbers[0];
+    var min = arrNumbers[0];
+    for(int i = 0; i < arrNumbers.Length; i++)
+    {
+        if(arrNumbers[i] >= max)
+        {
+            max = arrNumbers[i];
+        }
+        else if (arrNumbers[i] <= min)
+        {
+            min = arrNumbers[i];
+        }
+    }
+    diff = max - min;
+    Console.WriteLine("Разница между max элементом и min элементом: {0}", diff);
+}
+
+PrintArrNumbers();
+Console.WriteLine();
+DifferenceNum();
